aff09439e1199ea1baa8ab43603e467b94e07e1d
Wahyu Fatur Rizki <wahyufaturrizkyy@gmail.com>
2020-09-17 13:15:26 +0700

    task 17 Sept 2020

--- src/screens/bantuan/bantuanLain.js
+++ src/screens/bantuan/bantuanLain.js
@@ -1,92 +1,141 @@
 /* eslint-disable */
-import React, { useState } from 'react';
-import { Text, View, Image, ImageBackground, TouchableOpacity, Dimensions } from 'react-native';
-import LinearGradient from 'react-native-linear-gradient'
-import {TextInput} from 'react-native-paper'
-import AsyncStorage from '@react-native-community/async-storage'
+import React, {useState} from 'react';
+import {
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import {TextInput} from 'react-native-paper';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import Appbar from '../../components/appbarHome';
-import { ScrollView } from 'react-native-gesture-handler';
-import {URL, formatRupiah} from '../../utils/global'
+import {ScrollView} from 'react-native-gesture-handler';
+import {URL, formatRupiah} from '../../utils/global';
 
 function bantuanLain(props) {
-    const [masalah, setMasalah] = useState("")
-    const [changeBantu, setChangeBantu] = useState(0)
-    const { height, width } = Dimensions.get("window");
+  const [masalah, setMasalah] = useState('');
+  const [changeBantu, setChangeBantu] = useState(0);
+  const {height, width} = Dimensions.get('window');
 
-    const handleProblem = async() => {
-        let formdata = new FormData();
-        formdata.append("problem", masalah)
-        const value = await AsyncStorage.getItem('data');
-        const data = JSON.parse(value)
-        let requestOptions = {
-            method: 'POST',
-            body: formdata,
-            headers: {
-                Authorization: `Bearer ${data.token}`,
-                'Content-Type': 'multipart/form-data'
-            }
-        };
+  const handleProblem = async () => {
+    let formdata = new FormData();
+    formdata.append('problem', masalah);
+    const value = await AsyncStorage.getItem('data');
+    const data = JSON.parse(value);
+    let requestOptions = {
+      method: 'POST',
+      body: formdata,
+      headers: {
+        Authorization: `Bearer ${data.token}`,
+        'Content-Type': 'multipart/form-data',
+      },
+    };
 
-        fetch(URL+"/v1/helpdesk", requestOptions)
-            .then((response) => response.json())
-            .then( async(responseData) => {
-               if(responseData.message === "Success"){
-                   alert('Berhasil kirim data')
-               }else{
-                   alert('Ada kesalahan, mohon ulang')
-               }
-            })
-            .done();
-    }
-    return (
-        <View style={{flex:1}}>
-           <Appbar params={props}/>
-            
-                <ScrollView>
+    fetch(URL + '/v1/helpdesk', requestOptions)
+      .then(response => response.json())
+      .then(async responseData => {
+        if (responseData.message === 'Success') {
+          alert('Berhasil kirim data');
+          props.navigation.navigate('JualanAnda', {
+            title: 'Jualan Anda',
+          });
+        } else {
+          alert('Ada kesalahan, mohon ulang');
+        }
+      })
+      .done();
+  };
+  return (
+    <View style={{flex: 1}}>
+      <Appbar params={props} />
 
-                    <View style={{alignItems:'center', flex:1, width:'90%', alignSelf:'center'}}>
+      <ScrollView>
+        <View
+          style={{
+            alignItems: 'center',
+            flex: 1,
+            width: '90%',
+            alignSelf: 'center',
+          }}>
+          <Image
+            source={require('../../assets/images/ask.png')}
+            style={{
+              width: width * 0.2,
+              height: width * 0.2,
+              resizeMode: 'contain',
+              marginBottom: height * 0.02,
+              marginTop: 20,
+            }}
+          />
 
-                        <Image
-                            source={require('../../assets/images/ask.png')}
-                            style={{width:width*0.2, height:width*0.2, resizeMode:'contain', marginBottom:height*0.02, marginTop: 20}}
-                        />
+          <View
+            style={{alignItems: 'center', width: '90%', alignSelf: 'center'}}>
+            <Text
+              style={{
+                fontSize: 26,
+                fontWeight: 'bold',
+                marginBottom: height * 0.02,
+                textAlign: 'center',
+              }}>
+              Tulis Masalah Anda di Sini
+            </Text>
+          </View>
 
-                        <View style={{alignItems:'center', width:'90%', alignSelf:'center'}}>
-                            <Text style={{fontSize:26, fontWeight:'bold', marginBottom:height*0.02, textAlign:'center'}}>Tulis Masalah Anda di Sini</Text>  
-                        </View>
-
-                        <TextInput
-                            label='Masalah Detail'
-                            value={masalah}
-                            mode = "outlined"
-                            onChangeText={(val)=> setMasalah(val)}
-                            style={{width:'100%', alignSelf:'center',  backgroundColor:'white', marginTop:height*0.005, marginBottom:height*0.01}}
-                            multiline={true}
-                            numberOfLines={6}
-                        />
-                    
-                    </View>
-
-                    <View style={{flex:1, width:'90%', alignSelf:'center', marginBottom:height*0.01}}>
-                        <Text>Note :</Text>
-                        <Text>Kami akan menghubungi Anda 1 x 24 jam</Text>
-                    </View>
-
-                    <View style={{alignItems:'center'}}>
-                        <TouchableOpacity style={{width:'90%', alignSelf:'center'}} onPress={() => handleProblem()}>
-                            <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 1}} colors={['#0956C6', '#0879D8', '#07A9F0']}
-                                style={{padding:height*0.02, justifyContent:'center', alignItems:'center', borderRadius:10}}
-                            >
-                                <Text style={{fontSize:20, textAlign:'center', color:'white'}}>
-                                    Kirim
-                                </Text>
-                            </LinearGradient>
-                        </TouchableOpacity>
-                    </View>
-                </ScrollView>
+          <TextInput
+            label="Masalah Detail"
+            value={masalah}
+            mode="outlined"
+            onChangeText={val => setMasalah(val)}
+            style={{
+              width: '100%',
+              alignSelf: 'center',
+              backgroundColor: 'white',
+              marginTop: height * 0.005,
+              marginBottom: height * 0.01,
+            }}
+            multiline={true}
+            numberOfLines={6}
+          />
         </View>
-    );
+
+        <View
+          style={{
+            flex: 1,
+            width: '90%',
+            alignSelf: 'center',
+            marginBottom: height * 0.01,
+          }}>
+          <Text>Note :</Text>
+          <Text>Kami akan menghubungi Anda 1 x 24 jam</Text>
+        </View>
+
+        <View style={{alignItems: 'center'}}>
+          <TouchableOpacity
+            style={{width: '90%', alignSelf: 'center'}}
+            onPress={() => handleProblem()}>
+            <LinearGradient
+              start={{x: 0, y: 0}}
+              end={{x: 1, y: 1}}
+              colors={['#0956C6', '#0879D8', '#07A9F0']}
+              style={{
+                padding: height * 0.02,
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: 10,
+              }}>
+              <Text style={{fontSize: 20, textAlign: 'center', color: 'white'}}>
+                Kirim
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
 
 export default bantuanLain;
--- src/screens/tarik/saldoPenjual.js
+++ src/screens/tarik/saldoPenjual.js
@@ -1,88 +1,135 @@
 /* eslint-disable */
 import React, {useState, useEffect} from 'react';
-import { View, Text, Dimensions, Image } from 'react-native'
-import {URL, formatRupiah} from '../../utils/global'
-import AsyncStorage from '@react-native-community/async-storage'
+import {View, Text, Dimensions, Image} from 'react-native';
+import {URL, formatRupiah} from '../../utils/global';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import Appbar from '../../components/appbarHome'
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import Loading from '../../components/loading'
+import Appbar from '../../components/appbarHome';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Loading from '../../components/loading';
 
 function saldoPenjual(props) {
-    const [saldo, setSaldo] = useState("0")
-    const [loading,setLoading] = useState(true)
+  const [saldo, setSaldo] = useState('0');
+  const [loading, setLoading] = useState(true);
 
-    const { height, width } = Dimensions.get("window");
-    const urlSaldo = URL+"v1/saldo/my" 
+  const {height, width} = Dimensions.get('window');
+  const urlSaldo = URL + 'v1/saldo/my';
 
+  useEffect(() => {
+    getSaldo();
+  }, []);
 
-    useEffect(() => {
-        getSaldo()
-    }, [])
+  const gotoPenarikan = () => {
+    props.navigation.navigate('Penarikan', {title: 'Penarikan'});
+  };
 
-    const gotoPenarikan = () => {
-        props.navigation.navigate('Penarikan',{title:"Penarikan"})      
-    }
+  const gotoPembayaran = () => {
+    props.navigation.navigate('Transaksi', {title: 'Transaksi Saya'});
+  };
 
-    const gotoPembayaran = () => {
-        props.navigation.navigate("Transaksi", {title:"Transaksi Saya"})
-    }
+  const getSaldo = async () => {
+    setLoading(true);
+    const value = await AsyncStorage.getItem('data');
+    const data = JSON.parse(value);
 
-    const getSaldo = async() => {
-        setLoading(true)
-        const value = await AsyncStorage.getItem('data');
-        const data = JSON.parse(value)
+    let headers = {
+      Authorization: `Bearer ${data.token}`,
+      'Access-Control-Allow-Origin': '*',
+    };
 
-        let headers = {
-            Authorization: `Bearer ${data.token}`,
-            'Access-Control-Allow-Origin': '*',
-        }
+    fetch(urlSaldo, {headers})
+      .then(response => response.json())
+      .then(responseData => {
+        setLoading(false);
+        console.log('saldoPenjual', responseData);
+        setSaldo(responseData.data);
+        console.log(responseData.data);
+      })
+      .catch(e => console.log(e));
+  };
 
-        fetch(urlSaldo, {headers})
-            .then(response => response.json())
-            .then(responseData => {
-                setLoading(false)
-                setSaldo(responseData.data)
-                console.log(responseData.data)
-            })
-            .catch(e => console.log(e))
-    }
+  return (
+    <View style={{flex: 1}}>
+      <Appbar params={props} />
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 30,
+          backgroundColor: '#93DCFC',
+        }}>
+        <Text style={{marginBottom: height * 0.01}}>Saldo</Text>
+        <Text
+          style={{
+            marginBottom: 10,
+            fontSize: 20,
+            fontWeight: 'bold',
+            color: '#0A56C3',
+            width: '100%',
+            alignSelf: 'center',
+            textAlign: 'center',
+          }}>
+          Rp. {formatRupiah(saldo)}
+        </Text>
+      </View>
 
-    return (
-        <View style={{flex:1}}>
-            <Appbar params={props}/>
-            <View style={{justifyContent:'center', alignItems:'center', padding:30, backgroundColor:'#93DCFC'}}>
-                <Text style={{marginBottom:height*0.01}}>Saldo</Text>
-                <Text style={{marginBottom:10, fontSize:20, fontWeight:'bold', color:'#0A56C3', width: '100%', alignSelf:'center', textAlign:'center'}}>Rp. {formatRupiah(saldo)}</Text>
-            </View>
-
-            <View style={{flex:1, flexDirection:'row', justifyContent:'space-around', padding:15}}>
-                <View style={{backgroundColor:'white', padding:20, width:width*0.4, height:height*0.18}}>
-                    <TouchableOpacity onPress={gotoPenarikan} style={{justifyContent:'center', alignItems:'center'}}>
-                        <Image 
-                            source={require('../../assets/images/penarikan.png')}
-                            style={{width:50,height:50,marginBottom:height*0.01, resizeMode:"contain"}}
-                        />
-                        <Text>Penarikan</Text>
-                    </TouchableOpacity>
-                </View>
-
-                <View style={{backgroundColor:'white', padding:20, width:width*0.4, height:height*0.18}}>
-                    <TouchableOpacity onPress={gotoPembayaran} style={{justifyContent:'center', alignItems:'center'}}>
-                        <Image 
-                            source={require('../../assets/images/transaksi.png')}
-                            style={{width:50,height:50,marginBottom:height*0.01, resizeMode:"contain"}}
-                        />
-                        <Text>Transaksi</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-
-            {loading &&
-                <Loading/>
-            }
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          padding: 15,
+        }}>
+        <View
+          style={{
+            backgroundColor: 'white',
+            padding: 20,
+            width: width * 0.4,
+            height: height * 0.18,
+          }}>
+          <TouchableOpacity
+            onPress={gotoPenarikan}
+            style={{justifyContent: 'center', alignItems: 'center'}}>
+            <Image
+              source={require('../../assets/images/penarikan.png')}
+              style={{
+                width: 50,
+                height: 50,
+                marginBottom: height * 0.01,
+                resizeMode: 'contain',
+              }}
+            />
+            <Text>Penarikan</Text>
+          </TouchableOpacity>
         </View>
-    );
+
+        <View
+          style={{
+            backgroundColor: 'white',
+            padding: 20,
+            width: width * 0.4,
+            height: height * 0.18,
+          }}>
+          <TouchableOpacity
+            onPress={gotoPembayaran}
+            style={{justifyContent: 'center', alignItems: 'center'}}>
+            <Image
+              source={require('../../assets/images/transaksi.png')}
+              style={{
+                width: 50,
+                height: 50,
+                marginBottom: height * 0.01,
+                resizeMode: 'contain',
+              }}
+            />
+            <Text>Transaksi</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {loading && <Loading />}
+    </View>
+  );
 }
 
 export default saldoPenjual;
